9a74a53152bd1402eeeeafd01743b2c04d272d01
Yng Fiji Wtr <167939409+yngfijiwtr@users.noreply.github.com>
2024-10-17 12:10:05 -0700

    Static IP Code Update
    Static IP Code Update

--- SensorDataLogging.ino
+++ SensorDataLogging.ino
@@ -41,6 +41,10 @@ int tempPin = 0;
 char ssid[] = SECRET_SSID;        // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
 
+IPAddress staticIP(10, 0, 0, 200); // Your desired static IP address
+IPAddress gateway(10, 0, 0, 1); // Your network gateway
+IPAddress subnet(255, 255, 255, 0); // Your subnet mask
+
 int wifiStatus = WL_IDLE_STATUS;
 WiFiServer server(80);
 WiFiUDP Udp; // A UDP instance to let us send and receive packets over UDP
@@ -341,6 +345,9 @@ void printWifiStatus() {
 }
 
 void connectToWiFi(){
+  WiFi.config(staticIP, gateway, subnet);
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
   // check for the WiFi module:
   if (WiFi.status() == WL_NO_MODULE) {
     Serial.println("Communication with WiFi module failed!");
@@ -369,6 +376,9 @@ void connectToWiFi(){
 }
 void wifiReconnect(){
   if(WiFi.status() != WL_CONNECTED){     //To prevent the Wifi Lease from expiring
+    WiFi.config(staticIP, gateway, subnet);
+    Serial.print("Connected with IP: ");
+    Serial.println(WiFi.localIP());
     wifiStatus = 0;                      //WL_CONNECTED = 3, didn't know the keyword for Disconnected
     while (wifiStatus != WL_CONNECTED) {                //Actually Reconnects
       Serial.print("Attempting to connect to SSID: ");
