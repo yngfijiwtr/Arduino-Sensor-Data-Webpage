59df7498264d3913a606946c2d9930ffdde20c4e
Yng Fiji Wtr <167939409+yngfijiwtr@users.noreply.github.com>
2025-06-28 13:10:15 -0700

    Update arduino_secrets.h

--- arduino_secrets.h
+++ arduino_secrets.h
@@ -1,2 +1,2 @@
-#define SECRET_SSID  "SSID"
-#define SECRET_PASS  "PASSWORD"
+#define SECRET_SSID  "SSID"        //Put your Wifi SSID here
+#define SECRET_PASS  "PASSWORD"    //Put your Wifi Password here
